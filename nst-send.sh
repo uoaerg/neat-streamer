843dcb4487543ee96bbfd423c2683fab4a82e988
Tom Jones <tom@erg.abdn.ac.uk>
2017-05-02 09:17:08 +0100

    Loop camera output and don't die on icmp

--- nst-send.sh
+++ nst-send.sh
@@ -5,7 +5,7 @@ set -e
 unamestr=`uname`
 
 if [ "$unamestr" == 'Linux' ]; then
-	raspivid -n -t 0 -o - | nc -u 127.0.0.1 6101 & 
+	raspivid -n -t 0 -o - | gst-launch-1.0 fdsrc ! udpsink host=127.0.0.1 port=6101 &
 fi
 
 ./neat-streamer -h 10.10.0.2 -d
