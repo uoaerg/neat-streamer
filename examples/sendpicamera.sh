12b5d5baf6d6934f5ccfc544f694fcd2dfbde23f
Tom Jones <tom@erg.abdn.ac.uk>
2017-05-02 10:37:56 +0100

    remove extra whitespace

--- examples/sendpicamera.sh
+++ examples/sendpicamera.sh
@@ -1,7 +1,7 @@
 raspivid -n -t 0 -o - | gst-launch-1.0 fdsrc ! udpsink host=127.0.0.1 port=6101 &  
 
-gst-launch-1.0 udpsrc port=6101 \              
-    ! h264parse \                              
-    ! rtph264pay config-interval=1 pt=96 \     
+gst-launch-1.0 udpsrc port=6101 \
+    ! h264parse \
+    ! rtph264pay config-interval=1 pt=96 \
     ! udpsink host=10.10.0.2 port=6969         
 exit
